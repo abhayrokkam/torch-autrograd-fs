f390d65e56f0541ed636575611fafcc093912c5f
abhayrokkam <abhayrokkam@gmail.com>
2024-03-22 14:35:32 +0800

    neural network representation, implement tanh, efficient backward propogation (_backward function within operations rather than a seperate function constantly comparing operation)

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 Learning Resource: [LINK](https://youtu.be/VMj-3S1tku0?si=qfz9OuDRKSzVn2nY)
 
-    Note: All the `helper_xyz` folders have code that were directly copied from the learning resource. 
+    Note: All the `helper_xyz` folders contain code that were directly copied from the learning resource. 
 
 ## Prerequisites
 - I start by importing the ***visualization functionality from the learning resource***. For this, I create a `helper_draw` folder from which I will be able to import `draw_dot` function from the learning resource.
 
 ## Basic Functionality
 - Create a `Value` class with basic functionalities of initialization, representation, addition and mulitplication.
-- Add the functionality of "memory" where the resulting `Value` item will remember the children `Value`/s involved in the operation used to produce it.
+- Add the functionality of memory where a `Value` item will remember the ***`Value`/s*** and the ***operation*** used to produce the item.
 
 ## Gradients
+- Create a `back_propogation` function in the `Value` class where each `Value`'s gradient is calculated with respect to the end-value. The process starts from end-value and goes towards the initial input values.
--- value.py
+++ value.py
@@ -1,18 +1,59 @@
+import math
+
 class Value:
     def __init__(self, data, label = '', _op = '', _prev = (), grad = 0):
         self.data = data
         self.label = label
         self._op = _op
-        self._prev = _prev
+        self._prev = list(_prev)
         self.grad = grad
+        self._backward = lambda : None
     
+    # Representation of class while print()
     def __repr__(self):
         return f'Value(data:{self.data}, label:{self.label})'
     
     def __add__(self, other):
         sum = self.data + other.data
-        return Value(sum, _op = '+', _prev=(self, other))
+        val = Value(sum, _op = '+', _prev=(self, other))
+        
+        def _backward():
+            self.grad = val.grad
+            other.grad = val.grad
+        val._backward = _backward
+        
+        return val
     
     def __mul__(self, other):
         prod = self.data * other.data
-        return Value(prod, _op = '*', _prev=(self, other))
+        val = Value(prod, _op = '*', _prev=(self, other))
+        
+        def _backward():
+            self.grad = other.data * val.grad
+            other.grad = self.data * val.grad
+        val._backward = _backward
+        
+        return val
+    
+    def tanh(self):
+        res = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
+        val = Value(res, _op = 'tanh', _prev = (self, ))
+        
+        def _backward():
+            self.grad = (1 - res**2) * val.grad
+        val._backward = _backward
+        
+        return val
+    
+    def back_prop(self):
+        final_to_initial = []
+        
+        def store_children(x):
+            final_to_initial.append(x)
+            for child in x._prev:
+                store_children(child)
+        
+        store_children(self)
+        self.grad = 1
+        for ele in final_to_initial:
+            ele._backward()
